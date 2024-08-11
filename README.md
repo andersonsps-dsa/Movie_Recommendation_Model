480df46a3bb8b13fe8095b7fc1aadf1058edd7e2
Anderson SPS DSA <andersonsps.dsa@gmail.com>
2024-08-11 01:53:06 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Movie_Recommendation_Model
- Movie Recommendation Model
+# Movie Recommendation Model 🎥
+
+Welcome to the **Movie Recommendation Model** repository! 🚀 This project focuses on developing a recommendation system using **Machine Learning**, a powerful method in artificial intelligence for finding the most similar items based on user preferences. Whether you're a data scientist, machine learning enthusiast, or simply curious about recommendation systems, this project offers valuable insights and a hands-on approach to building a movie recommender system.
+
+## Business Applications 📊🏢
+
+The principles behind this movie recommendation model can be applied to various industries beyond entertainment:
+
+- **E-commerce:** Recommending products based on previous purchases or browsing history.
+- **Streaming Services:** Suggesting new content, such as music, videos, or books.
+- **Retail:** Personalizing shopping experiences by recommending clothing, electronics, or other items.
+- **Social Media:** Enhancing user engagement by suggesting friends, groups, or content to follow.
+
+By leveraging the power of **Machine Learning**, businesses can create personalized experiences that increase customer satisfaction and drive engagement. 🌟
+
+## 🌐 View:
+[Click here](https://predict-models-additional-employee.streamlit.app/).
---
+++ classification_model.py
@@ -0,0 +1,53 @@
+import pickle
+import streamlit as st
+import pandas as pd
+
+data_load_state = st.text('Data loading in progress... ⟳')
+movie_piv = pickle.load(open('movie.pkl','rb'))
+model_ = pickle.load(open('recommend_movie.pkl','rb'))
+data_load_state.text('Data uploaded successfully! ✅')
+
+st.title('🎥 Movie Recommendation Model ')
+st.write('➤ Please select the film title from the fields below 🎬:')
+
+col1, col2 = st.columns(2)
+with col1:
+    movie_titles = movie_piv.index.tolist()
+    
+    def filter_titles(input_text, titles_list):
+        filtered_titles = [title for title in titles_list if input_text.lower() in title.lower()]
+        return sorted(filtered_titles)
+
+    user_input = st.text_input('Enter a movie title to view suggestions:')
+    if user_input:
+        suggestions = filter_titles(user_input, movie_titles)
+        if suggestions:
+            st.write('Suggestions:')
+            for suggestion in suggestions:
+                st.write(suggestion)
+        else:
+            st.write('No movies found. Please try another title.')
+
+with col2:
+    if user_input and suggestions:
+        selected_movie = suggestions[0]        
+        try:
+            with open(f'{selected_movie}_neighbors.pkl', 'rb') as file:
+                distances, neighbors = pickle.load(file)
+        except FileNotFoundError:
+            from data import save_neighbors
+            save_neighbors(selected_movie)
+            with open(f'{selected_movie}_neighbors.pkl', 'rb') as file:
+                distances, neighbors = pickle.load(file)
+        st.write(f'Similar movies to "{selected_movie}":')
+        for i in range(len(neighbors[0])):
+            suggestion = movie_piv.index[neighbors[0][i]]
+            st.markdown(
+                f"""
+                <div style="text-align: center; font-size:20px;">
+                    {suggestion}
+                </div>
+                """, unsafe_allow_html=True
+            )
+            
+st.write('-------')
